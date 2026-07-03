ff44dbb4109d1d15b1c1f0b2458e8bc0f852c831
BE A STEREOTYPE <judiction@gmail.com>
2026-07-03 17:00:37 -0300

    Addicao Info Hotspots

---
+++ data.js
@@ -0,0 +1,326 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sala-de-estar",
+      "name": "Sala de Estar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.6648891463691733,
+        "pitch": 0.24088384162999432,
+        "fov": 1.448842290523763
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2681066432738248,
+          "pitch": 0.5414405711510213,
+          "rotation": 4.71238898038469,
+          "target": "3-mesa-de-trabalho"
+        },
+        {
+          "yaw": 1.2189665582823679,
+          "pitch": 0.21180229377829107,
+          "rotation": 0,
+          "target": "2-quarto-e-varanda"
+        },
+        {
+          "yaw": -2.3225560561610923,
+          "pitch": 0.3496338734847573,
+          "rotation": 1.5707963267948966,
+          "target": "1-cozinha"
+        },
+        {
+          "yaw": -2.1388092395053313,
+          "pitch": 0.10566067546729485,
+          "rotation": 0.7853981633974483,
+          "target": "4-banheiro"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.11391186255606556,
+          "pitch": 0.10238576824860424,
+          "title": "Informação Decoração",
+          "text": "Esta unidade decorada tem apenas a finalidade de ilustrar uma sugestão de decoração proposta para o objeto de venda. Móveis, marcenaria, bancadas, cubas, louças, metais, vidros, espelhos, utensílios, objetos de decoração, iluminação e sancas apresentados neste decorado são apenas sugestões de decoração, não integrando o contrato de compra e venda da unidade autônoma. Os revestimentos de pisos, paredes, forros e demais acabamentos apresentados nesta unidade decorada também compõe a sugestão de decoração. A unidade autônoma será entregue conforme o padrão disposto no Memorial Descritivo, que integra o instrumento de compra e venda. Os acabamentos poderão ser modificados conforme o programa My Choice."
+        },
+        {
+          "yaw": 1.7138351738423756,
+          "pitch": -0.4356893215280344,
+          "title": "Informação climatização",
+          "text": "O sistema de ar condicionado instalado nesta unidade decorada é apenas para sua comodidade e conforto.<div>O sistema de ar condicionado do empreendimento seguirá os padrões constantes no Memorial Descritivo que integra o instrumento de venda da unidade autônoma.</div>"
+        }
+      ],
+      "minimap": "assets/0-sala-de-estar.png"
+    },
+    {
+      "id": "1-cozinha",
+      "name": "Cozinha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.3865110961689169,
+        "pitch": 0.42279383566885187,
+        "fov": 1.448842290523763
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2805673706728626,
+          "pitch": 0.3502312068089566,
+          "rotation": 5.497787143782138,
+          "target": "0-sala-de-estar"
+        },
+        {
+          "yaw": 1.2656508138985263,
+          "pitch": 0.24087667390582723,
+          "rotation": 10.995574287564278,
+          "target": "3-mesa-de-trabalho"
+        },
+        {
+          "yaw": -1.7574234957209356,
+          "pitch": 0.13924167758691652,
+          "rotation": 1.5707963267948966,
+          "target": "4-banheiro"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5632027771365316,
+          "pitch": -0.043716871488491904,
+          "title": "Informação Portas",
+          "text": "As folhas das portas foram retiradas somente nesta unidade decorada. As unidades do empreendimento serão executadas obedecendo ao projeto executivo e seguindo rigorosamente as especificações contidas no memorial descritivo de construção e acabamentos."
+        },
+        {
+          "yaw": -1.5628872927611308,
+          "pitch": 0.08792475227068763,
+          "title": "Informação Elétrica",
+          "text": "Os quadros de luz, tomadas, interruptores e registros de comando das instalações hidráulicas poderão variar a localização em função da coluna, diferentes daqueles representados neste modelo."
+        },
+        {
+          "yaw": -1.5650809711856937,
+          "pitch": 0.20945183858740357,
+          "title": "Informação Molduras",
+          "text": "As molduras, pintura das portas e batentes apresentados nesta unidade decorada são apenas sugestão de decoração.<div>A unidade autônoma será entregue conforme o Memorial Descritivo, que integra o instrumento de compra e venda.</div>"
+        }
+      ],
+      "minimap": "assets/1-cozinha.png"
+    },
+    {
+      "id": "2-quarto-e-varanda",
+      "name": "Quarto e Varanda",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.8227065431415124,
+        "pitch": 0.24265546354961742,
+        "fov": 1.448842290523763
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9083241605594194,
+          "pitch": 0.4143207356075429,
+          "rotation": 1.5707963267948966,
+          "target": "3-mesa-de-trabalho"
+        },
+        {
+          "yaw": 2.9521100684987367,
+          "pitch": 0.20304329859330572,
+          "rotation": 7.0685834705770345,
+          "target": "0-sala-de-estar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7798714941106653,
+          "pitch": 0.026944204850551046,
+          "title": "<span style=\"color: rgb(227, 227, 227); font-family: &quot;Google Sans Flex&quot;, &quot;Google Sans&quot;, &quot;Helvetica Neue&quot;, sans-serif; font-size: 17px; font-variant-ligatures: none; white-space: pre-wrap; background-color: rgb(30, 31, 32);\">Informação Fachada</span>",
+          "text": "A representação da fachada nesta unidade decorada meramente ilustrativa e fictícia.<div>As fachadas do empreendimento serão executadas obedecendo ao projeto executivo e seguindo rigorosamente as especificações contidas no memorial descritivo de construção e acabamentos.</div>"
+        }
+      ],
+      "minimap": "assets/2-quarto-e-varanda.png"
+    },
+    {
+      "id": "3-mesa-de-trabalho",
+      "name": "Mesa de Trabalho",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.3984152107956547,
+        "pitch": 0.48505392775937395,
+        "fov": 1.448842290523763
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5182079181558752,
+          "pitch": 0.3423377735497102,
+          "rotation": 5.497787143782138,
+          "target": "2-quarto-e-varanda"
+        },
+        {
+          "yaw": -2.943651485169882,
+          "pitch": 0.5633324734281473,
+          "rotation": 1.5707963267948966,
+          "target": "0-sala-de-estar"
+        },
+        {
+          "yaw": -2.7874169475855126,
+          "pitch": 0.13097784513318445,
+          "rotation": 0.7853981633974483,
+          "target": "1-cozinha"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.541952794392217,
+          "pitch": -0.6559535935396532,
+          "title": "Informação climatização",
+          "text": "O sistema de ar condicionado instalado nesta unidade decorada é apenas para sua comodidade e conforto.<div>O sistema de ar condicionado do empreendimento seguirá os padrões constantes no Memorial Descritivo que integra o instrumento de venda da unidade autônoma.</div>"
+        },
+        {
+          "yaw": -1.7771543337079159,
+          "pitch": 0.08650965172141412,
+          "title": "Informação Decoração",
+          "text": "Esta unidade decorada tem apenas a finalidade de ilustrar uma sugestão de decoração proposta para o objeto de venda. Móveis, marcenaria, bancadas, cubas, louças, metais, vidros, espelhos, utensílios, objetos de decoração, iluminação e sancas apresentados neste decorado são apenas sugestões de decoração, não integrando o contrato de compra e venda da unidade autônoma.<br>Os revestimentos de pisos, paredes, forros e demais acabamentos apresentados nesta unidade decorada também compõe a sugestão de decoração. A unidade autônoma será entregue conforme o padrão disposto no Memorial Descritivo, que integra o instrumento de compra e venda. Os acabamentos poderão ser modificados conforme o programa My Choice.<br><br>"
+        }
+      ],
+      "minimap": "assets/3-mesa-de-trabalho.png"
+    },
+    {
+      "id": "4-banheiro",
+      "name": "Banheiro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.29160933962819513,
+        "pitch": 0.2926042704171685,
+        "fov": 1.448842290523763
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7154920357247008,
+          "pitch": 0.163824559631216,
+          "rotation": 4.71238898038469,
+          "target": "1-cozinha"
+        }
+      ],
+      "infoHotspots": [],
+      "minimap": "assets/4-banheiro.png"
+    }
+  ],
+  "name": "Be.in.Rio Praia Copacabana Coluna 3",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
